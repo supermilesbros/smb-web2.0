5272434573a651ac683306b81f6dc778039cf3c9
SuperMilesBros <jared@supermilesbros.com>
2021-09-01 19:53:37 -0400

    fine ill do it myself

--- components/ServiceWrapper.vue
+++ components/ServiceWrapper.vue
@@ -19,31 +19,31 @@
                         icon: "/images/icons/s1.png",
                         title: "Web Development",
                         desc: "Every brand needs a home and we can build it together. From quick turnaround templated solutions to a fully custom websites that accomadates your every need.",
-                        to: "/web-development"
+                        url: "/web-development"
                     },
                     {
                         icon: "/images/icons/s5.png",
                         title: "SEO",
                         desc: "Once you have a home online you need to help your potential clients find it. We know all the tricks to get you ranking high in online searches and beat the competition.",
-                        to: "/seo"
+                        url: "/seo"
                     },
                     {
                         icon: "/images/icons/s3.png",
                         title: "Brand Identity",
                         desc: "From logos to in-store signage we can help craft brand assets that will capture new and returning clients and make your branding something to remember.",
-                        to: "/brand-identity"
+                        url: "/brand-identity"
                     },
                     {
                         icon: "/images/icons/s4.png",
                         title: "Content Creation",
                         desc: "We create engaging content that cuts through the noise and reaches your audience.",
-                        to: "/content-creation"
+                        url: "/content-creation"
                     },
                     {
                         icon: "/images/icons/c2.png",
                         title: "Content Distribution",
                         desc: "Is your plate a little full? With a growing number of platforms wanting your attention, let us distibute your message for you.",
-                        to: "/content-creation"
+                        url: "/content-creation"
                     },
                     {
                         icon: "/images/icons/s2.png",
